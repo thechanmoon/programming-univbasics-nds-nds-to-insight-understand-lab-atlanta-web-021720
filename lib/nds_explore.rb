e369ed96f42d0127fc2e565998a0737185627096
CHANMOON PARK <thechanmoon@gmail.com>
2020-02-14 05:09:10 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -11,6 +11,6 @@ end
 def print_first_directors_movie_titles
   #pretty_print_nds(directors_database)
   directors_database[0][:movies].length.times do
-    puts directors_database[0][:movies].:title
+    puts directors_database[0][:movies][:title]
   end
 end
